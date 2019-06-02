846567b06e25ff550f03f5f8a10d6f194cbe53ee
cachaucanes23 <cachaucanes23@gmail.com>
2019-06-01 19:24:51 -0500

    index basico

---
+++ controller.js
@@ -0,0 +1,8 @@
+var app = angular.module("MyApp", [])
+    app.controller("MyAppController", function($scope){
+        
+
+        $scope.guardar = function(){
+            alert("hola")
+        }
+    })
--- todo.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('todoApp', [])
-  
-let ver = false;
-function guardar(){
-  alert("listo");
-}
